6bb91f794fd192ba10ecb04f6e12e70526b94d2c
nickmwangemi <nickmwangemi@gmail.com>
2020-07-21 03:25:51 +0300

    Added logic to cast vote

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const setupRedis = require('./setup/redis')
 const setupRouter = require('./setup/router')
 const setupDatabase = require('./setup/database')
 const setupMiddleware = require('./setup/middleware')
@@ -11,7 +12,9 @@ setupMiddleware(app)
 
 async function start() {
 	const db = await setupDatabase()
-	setupRouter(app, db)
+	const redisDb = await setupRedis()
+
+	setupRouter(app, db, redisDb)
 
 	app.listen(port, () => {
 		console.log(`Server is running on port ${port}`)
